c38b58fe63a580e5b6ea15462389c5a6c91575d3
joshlooks <joshua.looker@warwick.ac.uk>
2024-04-15 15:30:06 +0100

    updating stochastic parameters

--- stoch_model.R
+++ stoch_model.R
@@ -9,8 +9,8 @@ source(paste(here(), "/Models/stochastic_models.R", sep=""))
 total_time <- 0
 test_cao <- c()
 NRuns <- 20
-para <- list("lambda_u" = 0.015, "lambda_l"=0.015, "b_u"=1.9e-9, "b_l"=1.9e-7, "g"=4, "c"=2, "d"=10, "pu"=1, "pl"=1,
-             "kappa"=0.045, "gamma"=0.13, "k"=10, "w"=10, "delta"=1, "f"=1e-6, "r"=0.27, "D"=1, "a"=0.01)
+para <- list("b_u"=1.9e-9, "b_l"=1.9e-7, "g"=4, "c"=2, "d"=5.2, "pu"=1, "pl"=1,
+             "gamma"=0.15, "k"=20, "f"=2.8e-6, "r"=0.27, "D"=1, "a"=0.01)
 inits <- c(Tu=1e9, Eu=0, Iu=0, Vu=4e4, Tl=1e9, El=0, Il=0, Vl=0, X=0, Du=0, Dl=0)
 maxtime <- 30
 maxVl <- rep(0,NRuns)
