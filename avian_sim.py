75698dd54960ef4e555173e0f82051c48b5cc332
joshlooks <joshua.looker@warwick.ac.uk>
2024-04-17 11:24:03 +0100

    poster plots and init param values

--- avian_sim.py
+++ avian_sim.py
@@ -3,31 +3,56 @@ from Models import diffusion_advection
 import numpy as np
 from matplotlib import pyplot as plt
 
-ts = np.linspace(0, 30, 1000)
-inits = np.array([1e9, 0, 0, 40000, 1e9, 0, 0, 0, 0])
+Cell_Count_U = 4e8
+Cell_Count_L = 6.25e9
+ts = np.linspace(0, 10, 1000)
+
+inits = np.array([Cell_Count_U, 0, 0, 1.3e3, Cell_Count_L, 0, 0, 0, 0])
 # Parameters b_u, b_l, g, c, d, p_u, p_l, gamma, k, f, r, D, a
 # States are U1, E1, I1, V1, U2, E2, I2, V2, X
-p = [1.9e-9, 1.9e-7, 4, 2, 5.2, 10, 1, 0.15, 20, 2.8e-6, 0.27, 0.5, 0.01]
+p = [1.5e-8, 1.5e-6, 4, 2, 5.2, 5e7/(Cell_Count_U+Cell_Count_L), 5e7/(Cell_Count_U+Cell_Count_L), 0, 20, 0.56*2.8e-6, 0.27, 0.01, 0.1]
 results = simulate.solve_model(diffusion_advection.model, inits, ts, (p,))
 
-#Eclipsed, Infected in URT
-plt.plot(ts,results[:,1],label="EU")
-plt.plot(ts,results[:,2],label='IU')
-plt.legend()
-plt.show()
+experiment = False
 
-plt.plot(ts, results[:,0],label="SU")
-plt.legend()
-plt.show()
-plt.plot(ts, results[:,4],label="SL")
-plt.legend()
-plt.show()
+if (experiment):
+    #Eclipsed, Infected in URT
+    plt.plot(ts,results[:,1],label="EU")
+    plt.plot(ts,results[:,2],label='IU')
+    plt.legend()
+    plt.show()
+
+    plt.plot(ts, results[:,0],label="SU")
+    plt.legend()
+    plt.show()
+    plt.plot(ts, results[:,4],label="SL")
+    plt.legend()
+    plt.show()
 
 
-#VU and VL
-plt.plot(ts,results[:,3],label="VU")
-plt.legend()
-plt.show()
-plt.plot(ts,results[:,7],label="VL")
-plt.legend()
-plt.show()
+    #VU and VL
+    plt.plot(ts,results[:,3],label="VU")
+    plt.legend()
+    plt.show()
+    plt.plot(ts,results[:,7],label="VL")
+    plt.legend()
+    plt.show()
+else:
+    #VU and VL
+    plt.semilogy(ts, results[:,3], label="VU", color="#001380")
+    plt.semilogy(ts, results[:,7], label="VL", color="#800000")
+    plt.xlabel("Time (days)", fontsize=12)
+    plt.ylabel(r"Viral Titre (TCID$_{50}$/ml)", fontsize=12)
+    plt.legend(fontsize=12)
+    plt.savefig("Plots/VUvVL.pdf",dpi=1200,bbox_inches='tight')
+    plt.close()
+
+    #Dead and infected cells
+    plt.semilogy(ts, results[:,2], label="IU", color="#001380")
+    plt.semilogy(ts, results[:,6], label="IL", color="#800000")
+    plt.semilogy(ts, Cell_Count_U - results[:,0], label="DU", color="#6680FF")
+    plt.semilogy(ts, Cell_Count_L - results[:,4], label="DL", color="#FF8080")
+    plt.xlabel("Time (days)", fontsize=12)
+    plt.ylabel("Number of cells", fontsize=12)
+    plt.legend(fontsize=12)
+    plt.savefig("Plots/ID.pdf",dpi=1200,bbox_inches='tight')
--- stoch_model.R
+++ stoch_model.R
@@ -1,30 +1,55 @@
 # Load libraries
-library(here)
-library(adaptivetau)
+require(here)
+require(adaptivetau)
+require(matrixStats)
+require(tidyverse)
+require(latex2exp)
 
 # Load stochastic model
 source(paste(here(), "/Models/stochastic_models.R", sep=""))
 
-# Initialise total time and final sizes vector
-total_time <- 0
-test_cao <- c()
-NRuns <- 20
-para <- list("b_u"=1.9e-9, "b_l"=1.9e-7, "g"=4, "c"=2, "d"=5.2, "pu"=1, "pl"=1,
-             "gamma"=0.15, "k"=20, "f"=2.8e-6, "r"=0.27, "D"=1, "a"=0.01)
-inits <- c(Tu=1e9, Eu=0, Iu=0, Vu=4e4, Tl=1e9, El=0, Il=0, Vl=0, X=0, Du=0, Dl=0)
-maxtime <- 30
-maxVl <- rep(0,NRuns)
-maxVu <- rep(0,NRuns)
-Du <- rep(0,NRuns)
-Dl <- rep(0,NRuns)
+NRuns <- 1000
+TU <- 4e8
+TL <- 6.25e9
+para <- list("b_u"=1.5e-8, "b_l"=1.5e-6, "g"=4, "c"=2, "d"=5.2, "pu"=5e7/(TU+TL), "pl"=5e7/(TU+TL),
+             "gamma"=0, "k"=20, "f"=0.56*2.8e-6, "r"=0.27, "D"=0.01, "a"=0.1)
+inits <- c(Tu=TU, Eu=0, Iu=0, Vu=1.3e3, Tl=6.25e9, El=0, Il=0, Vl=0, X=0, Du=0, Dl=0)
+maxtime <- 10
+times <- seq(0,10,0.05)
+Vlres <- matrix(0,nrow=NRuns,ncol=length(times))
+Dlres <- matrix(0,nrow=NRuns,ncol=length(times))
+set.seed(1234)
 for(i in 1:NRuns){
-  # Run current realisation and record final size
-  # Also time how long the algorithm takes
-  start_time <- Sys.time()
+  # Run current realisation
   res = ssa.adaptivetau(inits, transitions_diffadv, lvrates_diffadv, para,
                         maxtime, tl.params = list(epsilon=0.05))
-  maxVl[i] <- max(res[,9])
-  maxVu[i] <- max(res[,5])
-  Du[i] <- res[dim(res)[1],11]
-  Dl[i] <-res[dim(res)[1],12]
+  Vlres[i,] <- approx(res[,1],res[,9], xout=times)$y
+  Dlres[i,] <- approx(res[,1],res[,12], xout=times)$y
 }
+
+Vlresquants <- colQuantiles(Vlres, probs = c(0.025,0.975))
+Vlresmed <- colQuantiles(Vlres, probs = 0.5)
+Dlresquants <- colQuantiles(Dlres, probs = c(0.025,0.975))
+Dlresmed <- colQuantiles(Dlres, probs = 0.5)
+
+Vltibble <- tibble(times,Vlresquants[,1],Vlresquants[,2],Vlresmed)
+colnames(Vltibble) <- c("t","LI","UI","Med")
+vlplot <- ggplot(Vltibble, aes(t, Med)) +                                      
+  geom_line(size = 1) + 
+  geom_ribbon(aes(ymin=LI, ymax=UI), alpha=0.1, fill = rgb(0.502,0,0,0.5),  
+              color = rgb(0.502,0,0), linetype = "dotted") + 
+  labs(x = "Time (days)",
+       y = TeX("Viral load (TCID$_{50}$)/ml")) +
+  scale_y_continuous(trans='log10') + theme_bw(base_size=20)
+ggsave("Plots/Vlstoch.pdf",plot = vlplot)
+
+Dltibble <- tibble(times,Dlresquants[,1],Dlresquants[,2],Dlresmed)
+colnames(Dltibble) <- c("t","LI","UI","Med")
+dlplot <- ggplot(Dltibble, aes(x=t,y=Med)) +                                      
+  geom_line(size = 1) + 
+  geom_ribbon(aes(ymin=LI, ymax=UI), alpha=0.1, fill = rgb(0.502,0,0,0.5),
+              color = rgb(0.502,0,0), linetype = "dotted") +
+  scale_y_continuous(trans='log10') + theme_bw(base_size=20) + 
+  labs(x = "Time (days)",
+       y = TeX("Dead cells"))
+ggsave("Plots/Dlstoch.pdf",plot = dlplot)
