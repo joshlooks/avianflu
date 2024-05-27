fc59bc1975bc43073a2afa69778ec26aaf9b559a
joshlooks <joshua.looker@warwick.ac.uk>
2024-05-27 17:54:56 +0100

    update plots

--- abc_vis.R
+++ abc_vis.R
@@ -2,12 +2,12 @@ library(GGally)
 library(tidyverse)
 library(here)
 require(latex2exp)
-source(paste0(here(),'/ABC/abc_helper.R'))
 post_tibble <- read_csv('Posterior/log_7.csv')
 colnames(post_tibble) <- c("bu","bl","pu","pl","gamma","D","a")
 post_density <- ggpairs(data=post_tibble,
                         columnLabels = c("beta[U]","beta[L]","p[U]","p[L]","gamma","D","a"), labeller="label_parsed",
-                        upper = list(continuous = "density"),
-                        lower = list(continuous = "density"),
-                        ) + theme(panel.spacing = unit(2, "lines"))
+                        upper = list(continuous = ggally_density),
+                        lower = list(continuous = ggally_density),
+                        diag = list(continuous = ggally_densityDiag)
+                        ) #+ theme(panel.spacing = unit(2, "lines"))
 ggsave("Plots/Param_posterior.pdf",plot = post_density, width = 40,  height = 40,  units = "cm")
